5178437da176122e2cbe08b32afe8bb2848a6081
X <phi@gmx.io>
2025-09-26 14:29:02 +0800

    update gmx timelock script

--- scripts/peripherals/deployGmxTimelock.js
+++ scripts/peripherals/deployGmxTimelock.js
@@ -6,11 +6,11 @@ const network = (process.env.HARDHAT_NETWORK || 'mainnet');
 const { AddressZero } = ethers.constants
 
 async function runForArbitrum() {
-  const admin = "0x49B373D422BdA4C6BfCdd5eC1E48A9a26fdA2F8b"
+  const admin = "0x2c247a44928d66041D9F7B11A69d7a84d25207ba"
   const rewardManager = { address: AddressZero }
   const buffer = 24 * 60 * 60
   const longBuffer = 7 * 24 * 60 * 60
-  const tokenManager = { address: "0x8D1d2e24eC641eDC6a1ebe0F3aE7af0EBC573e0D" }
+  const tokenManager = { address: "0x4bd1cdAab4254fC43ef6424653cA2375b4C94C0E" }
   const mintReceiver = { address: AddressZero }
   const maxTokenSupply = expandDecimals("13250000", 18)
 
@@ -26,7 +26,7 @@ async function runForArbitrum() {
 }
 
 async function runForAvax() {
-  const admin = "0x49B373D422BdA4C6BfCdd5eC1E48A9a26fdA2F8b"
+  const admin = "0x9bf98C09590CeE2Ec5F6256449754f1ba77d5aE5"
   const rewardManager = { address: ethers.constants.AddressZero }
   const buffer = 24 * 60 * 60
   const longBuffer = 7 * 24 * 60 * 60

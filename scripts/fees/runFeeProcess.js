d8caa3320b2c4d69f29f0ba5844f0abf0b9ea9d8
X <phi@gmx.io>
2025-11-18 10:10:07 +0800

    update scripts

--- scripts/fees/runFeeProcess.js
+++ scripts/fees/runFeeProcess.js
@@ -1,7 +1,11 @@
 const { createFeePlan } = require("./createFeePlanUtils")
+const { distributeFees } = require("./distributeFees")
+const { sendPushMessage } = require("../shared/helpers");
 
 async function main() {
+  await sendPushMessage("running fee process")
   await createFeePlan()
+  await distributeFees({ steps: "1,2,3,4,5,6" })
 }
 
 main()

3717884efb4f297ce9afdf01e03bfca8dac88604
X <phi@gmx.io>
2026-02-23 14:50:09 +0800

    update scripts

--- scripts/fees/distributeFees.js
+++ scripts/fees/distributeFees.js
@@ -329,7 +329,8 @@ async function updateGmxRewards() {
         // ExtendedGmxTracker
         rewardTracker: await contractAt("RewardTracker", "0x0755D33e45eD2B874c9ebF5B279023c8Bd1e5E93", feeKeepers.arbitrum),
         rewardToken: gmx.arbitrum,
-        transferAmount: bigNumberify(0) // feePlan.gmxRewards.arbitrum
+        transferAmount: bigNumberify(0), // feePlan.gmxRewards.arbitrum
+        customReceiver: "0x68863dDE14303BcED249cA8ec6AF85d4694dea6A"
       },
       // {
       //   // FeeGmxTracker
@@ -343,7 +344,8 @@ async function updateGmxRewards() {
         // ExtendedGmxTracker
         rewardTracker: await contractAt("RewardTracker", "0xB0D12Bf95CC1341d6C845C978daaf36F70b5910d", feeKeepers.avax),
         rewardToken: gmx.avax,
-        transferAmount: bigNumberify(0) // feePlan.gmxRewards.avax
+        transferAmount: bigNumberify(0), // feePlan.gmxRewards.avax
+        customReceiver: "0x0339740d92fb8BAf73bAB0E9eb9494bc0Df1CaFD"
       },
       // {
       //   // FeeGmxTracker
@@ -458,16 +460,16 @@ async function distributeFees({ write: _write, steps }) {
 
 
   if (shouldRunFeeStep(steps, 1)) {
-    // await withdrawFees();
-    // await printFeeHandlerBalances();
-    // saveFeeStep(1)
-    // await sendPushMessage("Step 1: Fees withdrawn")
+    await withdrawFees();
+    await printFeeHandlerBalances();
+    saveFeeStep(1)
+    await sendPushMessage("Step 1: Fees withdrawn")
   }
 
   if (shouldRunFeeStep(steps, 2)) {
-    // await bridgeTokens()
-    // saveFeeStep(2)
-    // await sendPushMessage("Step 2: Tokens bridged")
+    await bridgeTokens()
+    saveFeeStep(2)
+    await sendPushMessage("Step 2: Tokens bridged")
   }
 
   if (shouldRunFeeStep(steps, 3)) {

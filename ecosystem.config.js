97f145b24cb11bc157298cd05795655bb36045cb
X <phi@gmx.io>
2026-03-04 09:37:01 +0800

    update cron

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,3 @@
 module.exports = {
-  apps: [
-    {
-      name: "fee-process",
-      script: "./run-fee-process.js",
-      cron_restart: "0 1 * * 3",
-      timezone: "UTC",
-      autorestart: false,
-      watch: false,
-      interpreter: "/root/.nvm/versions/node/v22.21.1/bin/node"
-    }
-  ]
+  apps: []
 };
